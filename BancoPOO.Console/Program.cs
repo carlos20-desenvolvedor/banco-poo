cbff94783a0eccc54e6127d6c2cf79a2ab963403
carlossantos.desenvolvedor@gmail.com <carlossantos.desenvolvedor@gmail.com>
2020-05-11 08:35:49 -0300

    Cria paginas de registro e login

--- BancoPOO.Console/Program.cs
+++ BancoPOO.Console/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
+using BancoPOO.Pages.Usuario;
 namespace BancoPOO.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Bem vindo!");
+            //System.Console.Write("Gostaria de abrir uma conta? (Sim / Nao)");
+            //string resultado = System.Console.ReadLine();
+
+            bool opcaoinvalida = true;
+            while (opcaoinvalida == true)
+            {
+                System.Console.Write("Gostaria de abrir uma conta? (Sim / Nao)");
+                string resultado = System.Console.ReadLine();
+
+                if (resultado.ToLower() == "Sim".ToLower())
+                {
+                    opcaoinvalida = false;
+                    var register = new Register();
+                }
+                else if (resultado.ToLower() == "Nao".ToLower())
+                {
+                    opcaoinvalida = false;
+                    var login = new Login();
+                }
+                else
+                {
+                    opcaoinvalida = true;
+                }
+            }
+
+            
         }
     }
 }
